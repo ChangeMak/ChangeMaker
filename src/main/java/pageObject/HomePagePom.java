6b6ee21fc57a68a85ff3dfed8c8772b9b5764606
Viresh Borkar <vireshborkar22@gmail.com>
2022-07-29 14:48:40 +0530

    Added HomePagePom

---
+++ src/main/java/pageObject/HomePagePom.java
@@ -0,0 +1,12 @@
+package pageObject;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class HomePagePom {
+	
+	
+	@FindBy(how=How.XPATH,using="") private WebElement txt_mail;
+	
+}
